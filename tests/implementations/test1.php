9a116f4509d0ba4028f6a1d4ae6c742eaddbf66c
Sentgine <sentgine@gmail.com>
2024-05-12 23:16:42 +0800

    Updated QueryBuilder
    Fixed sanitizeSring method.

--- tests/implementations/test1.php
+++ tests/implementations/test1.php
@@ -13,7 +13,7 @@ function dump(array|string $data)
 
 function queryInfo(string $string)
 {
-    echo "<br/><strong>Query</strong>: {$string}";
+    echo "<br/><strong>Query <span style='color: green;'>(Correct)</span></strong>: {$string}";
 }
 
 function run(QueryBuilder $db, Closure $callable)
@@ -21,9 +21,9 @@ function run(QueryBuilder $db, Closure $callable)
     try {
         $callable();
     } catch (\Throwable $th) {
-        echo "<br/>This SQL has a syntax error:";
+        echo "<br><br/><strong>This SQL has a syntax error:</strong>";
         echo "<br/>Query (<span style='color:red;'>Error</span>): {$db->getLastQuery()}";
-        echo "<br/><br/>";
+        echo "<br/>";
     }
 }
 
@@ -37,17 +37,50 @@ $db->connect([
 
 run($db, function () use ($db) {
     $db->select('users');
-    $db->where('age', "30");
+    $db->where('age', '30.5 string');
     // $db->orWhere('name', 'cant "go"');
     $result = $db->get();
     echo queryInfo($db->getLastQuery());
-    dump($result);
+    //dump($result);
 });
 
 run($db, function () use ($db) {
     $db->select('users');
-    $db->where('name', 30, '<');
+    $db->where('age', 30.5);
+    // $db->orWhere('name', 'cant "go"');
     $result = $db->get();
     echo queryInfo($db->getLastQuery());
-    dump($result);
+    //dump($result);
+});
+
+run($db, function () use ($db) {
+
+    $db->select('users');
+    $db->where('age', "30");
+    // $db->orWhere('name', 'cant "go"');
+    $result = $db->get();
+    echo queryInfo($db->getLastQuery());
+    //dump($result);
+});
+
+run($db, function () use ($db) {
+    $name = <<<EOD
+        Can't go
+    EOD;
+    $db->select('users');
+    $db->where('name', $name);
+    $result = $db->get();
+    echo queryInfo($db->getLastQuery());
+    // dump($result);
+});
+
+run($db, function () use ($db) {
+    $name = <<<EOD
+        can't "go"
+    EOD;
+    $db->select('users');
+    $db->where('name', $name);
+    $result = $db->get();
+    echo queryInfo($db->getLastQuery());
+    // dump($result);
 });
