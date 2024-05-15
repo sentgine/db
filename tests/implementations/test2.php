8eb2bd247a8dfe6dcd42195dd8609892bd1d895c
Dodie Batoctoy <myfavoritecreator1@gmail.com>
2024-05-16 01:43:16 +0800

    testing scenarios

--- tests/implementations/test2.php
+++ tests/implementations/test2.php
@@ -3,21 +3,21 @@
 use Sentgine\Db\QueryBuilder;
 
 require_once __DIR__ . '/functions.php';
-
-$db = new QueryBuilder();
-$db->connect([
-    'host' => 'localhost',
-    'database' => 'test',
-    'username' => 'root',
-    'password' => ''
-]);
-
+//
+//$db = new QueryBuilder();
+//$db->connect([
+//    'host' => 'localhost',
+//    'database' => 'test',
+//    'username' => 'root',
+//    'password' => ''
+//]);
+//
 //$db->select('testtable', array("id","title"));
 //$db->nestWhereExpression(' 
 //    ( 
 //        {{nest1}}    
 //        OR 
-//         {{nest2}}
+//       {{nest2}}
 //    )
 //    AND
 //    (
@@ -38,12 +38,40 @@ $db->connect([
 //$db->where('age', '30.5', ">");
 //$db->andWhere('age', '30.5',"<");
 //echo $db->buildSQL();
+//
+//
+//
+//$db->select('dual', array("1 as col1", "\"col2\""));
+//$db->groupBy("col1");
+//$db->groupBy("col2");
+//$db->orderBy("col2","asc");
+//$db->orderBy("col1","desc");
+//echo $db->buildSQL();
 
 
 
-$db->select('dual', array("1 as col1", "\"col2\""));
-$db->groupBy("col1");
-$db->groupBy("col2");
-$db->orderBy("col2","asc");
-$db->orderBy("col1","desc");
-echo $db->buildSQL();
+//
+//$db = new QueryBuilder();
+//$db->connect([
+//    'host' => 'localhost',
+//    'database' => 'bup_ads',
+//    'username' => 'root',
+//    'password' => ''
+//]);
+//$db->select('page_campaign_data ', array("*"));
+//$db->nestWhereExpression(' 
+//    ( 
+//        {{nest1}}    
+//        OR
+//        {{nest2}}    
+//    )
+//    AND
+//    (
+//        {{nest3}}    
+//    )
+//    
+//    ');
+//
+//$db->where('id_page_campaign_data', 20,">=", expression: 'nest1');
+//$db->where('id_page_campaign_data', 20,">=", expression: 'nest1');
+//echo $db->buildSQL();
